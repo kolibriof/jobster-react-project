c56664650f674fdab852e93b0450f745d30b6cb3
kolibriof <kolibri380@gmail.com>
2023-05-02 15:44:46 +0200

    jobster: added search component

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -57,6 +57,12 @@ const allJobsSlice = createSlice({
     hideLoading: (state) => {
       state.isLoading = false;
     },
+    handleChange: (state, { payload: { name, value } }) => {
+      state[name] = value;
+    },
+    clearFilters: (state) => {
+      return { ...state, ...initialFilterState };
+    },
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -85,5 +91,6 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const { showLoading, hideLoading } = allJobsSlice.actions;
+export const { showLoading, hideLoading, handleChange, clearFilters } =
+  allJobsSlice.actions;
 export default allJobsSlice.reducer;
